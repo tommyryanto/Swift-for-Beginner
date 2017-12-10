661c2a7be4cbceb86858295662e4246b72ae13d4
Tommy Ryanto <tommyryanto@Tommys-MacBook-Air.local>
2017-12-10 15:24:29 +0700

    Initial Commit

---
+++ bljrswift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  bljrswift
+//
+//  Created by Tommy Ryanto on 10/12/17.
+//  Copyright © 2017 Tommy Ryanto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
